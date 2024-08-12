cbe493d6afbc2b4c08f3ed65406bebd081673f4f
Himanshu syuni <124446173+himanshusyuni@users.noreply.github.com>
2024-08-12 09:47:55 +0530

    Update Showtime.js

--- server/models/Showtime.js
+++ server/models/Showtime.js
@@ -14,13 +14,5 @@ const showtimeSchema = new mongoose.Schema({
 	isRelease: Boolean
 })
 
-showtimeSchema.pre('deleteOne', { document: true, query: true }, async function (next) {
-	const showtimeId = this._id
-	await this.model('User').updateMany(
-		{ 'tickets.showtime': showtimeId },
-		{ $pull: { tickets: { showtime: showtimeId } } }
-	)
-	next()
-})
 
 module.exports = mongoose.model('Showtime', showtimeSchema)
